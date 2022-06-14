2c769ee4066429ea06df19e28839aba035a862d0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-14 16:47:54 -0400

    create radius

--- Analogue Clock/clock.js
+++ Analogue Clock/clock.js
@@ -3,3 +3,6 @@ const canvas = document.getElementById('canvas');
 
 //create a 2d drawing object
 const ctx = canvas.getContext('2d');
+
+//calculate the clock radius by using the height
+const radius = canvas.height / 2;
