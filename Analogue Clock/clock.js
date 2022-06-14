bb0cdfa3662c708ff1cbca5af03fea396580f877
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-14 16:49:19 -0400

    reduce clock radius

--- Analogue Clock/clock.js
+++ Analogue Clock/clock.js
@@ -1,11 +1,14 @@
 //create a canvas object fro HTML element
-const canvas = document.getElementById('canvas');
+let canvas = document.getElementById('canvas');
 
 //create a 2d drawing object
-const ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d');
 
 //calculate the clock radius by using the height
-const radius = canvas.height / 2;
+let radius = canvas.height / 2;
 
 //remap the x and y axis to the center of the canvas
 ctx.translate(radius, radius);
+
+//reduce clock radius by 90%
+radius = radius * 0.90;
