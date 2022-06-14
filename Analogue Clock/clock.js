c00f488fa795637e5d69e27e20f27605184493a9
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-14 16:51:01 -0400

    add function drawFace

--- Analogue Clock/clock.js
+++ Analogue Clock/clock.js
@@ -20,3 +20,26 @@ function drawClock(){
   drawNumbers(ctx, radius);
   drawTime(ctx, radius);
 }
+
+function drawFace(ctx, radius){
+  let grad;
+  //draw white circle for the face
+  ctx.beginPath();
+  ctx.arc(0,0,radius,0,2*Math.PI);
+  ctx.fillStyle = "White";
+  ctx.fill();
+  // create a radial gradient (inner, middle, and outer edge of clock)
+  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  //define gradient as stroke style
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke(); 
+  //draw the center of the clock
+  ctx.beginPath();
+  ctx.arc(0,0, radius*0.1,0,2*Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+}
