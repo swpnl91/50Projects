b7cf8dba72d082649dc4ee7ca9499b16a962909d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-14 16:52:29 -0400

    add function drawTime

--- Analogue Clock/clock.js
+++ Analogue Clock/clock.js
@@ -61,3 +61,26 @@ function drawNumbers(ctx, radius) {
       ctx.rotate(-ang);
   }
 }
+
+function drawTime(ctx, radius){
+  let now = new Date();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+  //hour
+  hour = hour%12;
+  //calculate angle of hour hand
+  hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+  //make hour hand 50% of canvas's radius
+  drawHand(ctx, hour, radius*0.5, radius*0.07);
+  //minute
+  //calculate angle of minute hand
+  minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+  //make minute hand 80% of canvas's radius
+  drawHand(ctx, minute, radius*0.8, radius*0.07);
+  //second
+  //calculate angle of second hand
+  second=(second*Math.PI/30);
+  //make second hand 90% of canvas's radius
+  drawHand(ctx, second, radius*0.9, radius*0.02);
+}
