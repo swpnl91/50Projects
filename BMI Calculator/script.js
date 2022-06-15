7b65345a8e5e3ea42fe7b014b814c40c289b9b0a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-15 06:04:11 -0400

    create height

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -2,8 +2,14 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', function(e){
   e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
   
   
   
   
 });
+
+//notes
+//NaN !== NaN, use the isNaN() function
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
