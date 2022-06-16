b135908df9b3b321c28b2b619524986295f34079
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-16 18:12:10 -0400

    add condition for grey

--- Background Color Switcher/app.js
+++ Background Color Switcher/app.js
@@ -6,6 +6,9 @@ console.log(buttons);
 buttons.forEach(function(button){
   button.addEventListener('click', function(e){
     console.log(e.target);
+    if (e.target.id === 'grey'){
+      body.style.backgroundColor = e.target.id;
+    }
     
   })
 })
