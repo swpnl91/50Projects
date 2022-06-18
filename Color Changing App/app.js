b6c38f97a9c979e4449ed41ac34163b15d549387
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-18 06:06:34 -0400

    create button

--- Color Changing App/app.js
+++ Color Changing App/app.js
@@ -1,2 +1,5 @@
 //Select 7 colors
 let colors = [ 'red', 'blue', 'green', 'orange', 'purple', 'black', 'yellow'];
+
+//change the background of canvas when button is clicked
+let button = document.getElementById('button');
