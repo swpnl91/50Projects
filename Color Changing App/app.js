c001d0020485e5fc35591e8b205e82a910fcce16
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-18 06:09:30 -0400

    create canvas

--- Color Changing App/app.js
+++ Color Changing App/app.js
@@ -7,5 +7,7 @@ let button = document.getElementById('button');
 button.addEventListener('click', function(){
   //select a random number between 0 - 6
   let index = parseInt((Math.random()*colors.length)+1);
+  //grab the canvas
+  let canvas = document.getElementById('canvas');
   
 })
