8ff3e3f0e2587b302bf1dbd582db356177bc1b9b
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-18 06:10:12 -0400

    change canvas background

--- Color Changing App/app.js
+++ Color Changing App/app.js
@@ -9,5 +9,6 @@ button.addEventListener('click', function(){
   let index = parseInt((Math.random()*colors.length)+1);
   //grab the canvas
   let canvas = document.getElementById('canvas');
-  
+
+  canvas.style.background = `${colors[index]}`
 })
