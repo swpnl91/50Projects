5241508c52fe5ca70a6eaf734c44cd05a9fb607c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-20 06:01:25 -0400

    add event listener for sendButton

--- Contact Form Project/app.js
+++ Contact Form Project/app.js
@@ -15,3 +15,19 @@ resetButton.addEventListener('click', function(){
   email.value = '';
   message.value = '';
 })
+
+sendButton.addEventListener('click', function(e){
+  let name = document.getElementById('name');
+  let email = document.getElementById('email');
+  let message = document.getElementById('message');
+
+  name = name.value;
+  localStorage.setItem('name', name);
+  
+  email = email.value;
+  localStorage.setItem('email', email);
+
+  message = message.value;
+  localStorage.setItem('message', message);
+  
+})
