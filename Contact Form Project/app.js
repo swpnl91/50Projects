aa9053b35ae73726ac3858a3a5b4056812604960
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-20 06:00:31 -0400

    add event listener for form

--- Contact Form Project/app.js
+++ Contact Form Project/app.js
@@ -1,3 +1,7 @@
 let sendButton = document.getElementById('send');
 let resetButton = document.getElementById('reset');
 let form = document.getElementById('form');
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+})
