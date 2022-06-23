5bf9020977467bc80166fbda1c47cef8214bf4a1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-06-23 06:23:54 -0400

    create minutes

--- Countdown Timer/app.js
+++ Countdown Timer/app.js
@@ -1,4 +1,5 @@
 function getTimeRemaining(endtime){
   const t = Date.parse(endtime) - Date.parse(new Date()); // get total difference in seconds
   const seconds = Math.floor((t / 1000) % 60); // convert total seconds to number of seconds by diving total seconds by number of seconds remaining after days,hours, and minutes are account for
+  const minutes = Math.floor((t / 1000 /60) % 60); // convert total seconds to number of minutes by diving total seconds by number of minutes remaining after days and hours are account for
 }
