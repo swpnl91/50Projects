ce3d3063df9dcb06878372f197c58ba10b872079
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-04 14:51:07 -0400

    create x

--- Event Countdown Timer/app.js
+++ Event Countdown Timer/app.js
@@ -1,3 +1,8 @@
 //Set date to countdown to:
 let countDownFromDate = new Date('Feb 1, 2020 12:00:00').toDateString();
 let countDownDate = new Date('Feb 1, 2020 12:00:00').getTime();
+
+let x = setInterval(function(){
+  
+  
+}, 1000);
