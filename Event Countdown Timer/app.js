c6c0555da8cfc039f2a16d7aa70788fb40a5c1d3
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-04 14:52:55 -0400

    create days, hours, minutes, seconds

--- Event Countdown Timer/app.js
+++ Event Countdown Timer/app.js
@@ -6,6 +6,11 @@ let x = setInterval(function(){
   let now = new Date().getTime();
   //get distance between current date to countdown date
   let distance = countDownDate - now;
+  //get days, hours, minutes, and seconds
+  let days = Math.floor(distance / (1000 * 60 *60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 *60*24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60))/ (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
   
 }, 1000);
