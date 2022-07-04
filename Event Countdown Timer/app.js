51916c3d832bfdc3e116e987aeb4be822c1aef3f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-04 14:53:43 -0400

    change countdown-form

--- Event Countdown Timer/app.js
+++ Event Countdown Timer/app.js
@@ -11,6 +11,8 @@ let x = setInterval(function(){
   let hours = Math.floor((distance % (1000 * 60 *60*24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60))/ (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //output countdown from date
+  document.getElementById('countdown-from').innerHTML = `${countDownFromDate}`
   
   
 }, 1000);
