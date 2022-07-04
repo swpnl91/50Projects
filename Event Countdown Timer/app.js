30be50f5622c5bc0184c88ed11bcd292ffd670cb
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-04 14:52:10 -0400

    create distance

--- Event Countdown Timer/app.js
+++ Event Countdown Timer/app.js
@@ -4,6 +4,8 @@ let countDownDate = new Date('Feb 1, 2020 12:00:00').getTime();
 
 let x = setInterval(function(){
   let now = new Date().getTime();
+  //get distance between current date to countdown date
+  let distance = countDownDate - now;
   
   
 }, 1000);
