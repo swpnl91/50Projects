3e8add9a4280de88f00187d5a998dc17810b568e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-05 10:41:42 -0400

    add conditions

--- Greeting Message Project/app.js
+++ Greeting Message Project/app.js
@@ -4,3 +4,11 @@ let hrs = today.getHours();
 
 document.writeln('<b>');
 document.writeln('<b>');
+
+if (hrs <= 12){
+  document.write('Good Morning, '+name+'!');
+} else if (hrs <= 18){
+  document.write('Good Afternoon, '+name+'!');
+} else {
+  document.write('Good Evening, '+name+'!');
+}
