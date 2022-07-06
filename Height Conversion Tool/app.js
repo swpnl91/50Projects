b6843ca9bafbbc44dfae7aea8f5252e5e3fa10a4
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-06 11:07:41 -0400

    add conditions

--- Height Conversion Tool/app.js
+++ Height Conversion Tool/app.js
@@ -11,4 +11,18 @@ form.addEventListener('submit', function(e){
   feet = parseInt(feet.value);
   inches = parseInt(inches.value);
 
+  if (isNaN(feet) || isNaN(inches)){
+    results.textContent = "Please enter a valid number!";
+  } else if (feet < 0 ) {
+    results.textContent = "Please enter a feet value > 0";
+  } else if (inches < 0 || inches >= 12) {
+    results.textContent = "Please enter an inch value between 0 and 12";
+  } else {
+    //make conversion to centimers
+    //cm = inches * 2.54
+    let totalInches = (feet*12) + inches;
+    results.textContent = `${totalInches} cm`;
+    document.querySelector('#feet').value = '';
+    document.querySelector('#inches').value = '';
+  }
 })
