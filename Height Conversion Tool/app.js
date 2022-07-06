892a4327cb2ee1a4510bfbdc5be8c23c8f84f08e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-06 11:04:40 -0400

    create inches

--- Height Conversion Tool/app.js
+++ Height Conversion Tool/app.js
@@ -3,5 +3,6 @@ const form = document.querySelector('form');
 form.addEventListener('submit', function(e){
   //grab content from feet input
   let feet = document.querySelector('#feet');
+  let inches = document.querySelector('#inches');
 
 })
