730aac8583ee9cfe1b5555590bd8d6eeab4166a3
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-07 12:01:22 -0400

    add EventListener for window

--- Interactive Quiz Project/app.js
+++ Interactive Quiz Project/app.js
@@ -29,3 +29,45 @@ const questions = [
 let currentQuestion = 0;
 let correctAnswers = 0;
 let quizOver = false;
+
+window.addEventListener('DOMContentLoaded', function(e){
+  displayCurrentQuestion();
+
+  var quizMessage = document.querySelector('.quizMessage');
+
+      quizMessage.style.display = 'none';
+
+  document.querySelector('.nextButton').addEventListener('click', function(){
+      
+      if(!quizOver){
+          var radioBtnsChecked = document.querySelector('input[type=radio]:checked');
+
+          if (radioBtnsChecked === null){
+              quizMessage.innerText = 'Please select an answer';
+              quizMessage.style.display = 'block';
+          } else {
+              console.log(radioBtnsChecked.value);
+              quizMessage.style.display = 'none';
+              if (parseInt(radioBtnsChecked.value) === questions[currentQuestion].correctAnswer){
+                  correctAnswers++;
+              }
+
+              currentQuestion++;
+
+              if (currentQuestion < questions.length){
+                  displayCurrentQuestion();
+              } else {
+                  displayScore();
+                  document.querySelector('.nextButton').innerText = 'Play Again?';
+                  quizOver = true;
+               }
+              }   
+      } else {
+          quizOver = false;
+          document.querySelector('.nextButton').innerText = 'Next Question';
+          resetQuiz();
+          displayCurrentQuestion();
+          hideScore();
+      }
+  });
+});
