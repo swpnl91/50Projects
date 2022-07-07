9a02731865f46db19f9c3dfaa3ac55733aaf9b4c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-07 11:58:54 -0400

    create questions

---
+++ Interactive Quiz Project/app.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+      question: 'What is the baby of a moth known as?',
+      choices: ['baby', 'infant', 'kit', 'larva'],
+      correctAnswer: 3
+  },
+  {
+      question: 'What is the adult of a kid called?',
+      choices: ['calf', 'doe', 'goat', 'chick'],
+      correctAnswer: 2
+  },
+  {
+      question: 'What is the young of buffalo called?',
+      choices: ['calf', 'baby', 'pup', 'cow'],
+      correctAnswer: 0
+  },
+  {
+      question: 'What is a baby alligator called?',
+      choices: ['baby', 'gator', 'hatchling', 'calf'],
+      correctAnswer: 1
+  },
+  {
+      question: 'What is a baby goose called?',
+      choices: ['gooser', 'gosling', 'gup', 'pup'],
+      correctAnswer: 1
+  }
+];
