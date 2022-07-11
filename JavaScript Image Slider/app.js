a0446252137b77ba1e3393a7e98810bec7db7296
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-11 11:32:59 -0400

    add event listener to arrows

--- JavaScript Image Slider/app.js
+++ JavaScript Image Slider/app.js
@@ -14,5 +14,20 @@ canvas.style.background = `url(./images/${images[0]})`
 let arrows = document.querySelectorAll('.arrow');
 
 arrows.forEach(function(arrow){
-  
+  arrow.addEventListener('click', function(e){
+    if (e.target.id === "left"){
+      //check to see if at beginning of array
+      i--;
+      if (i < 0){
+        i = images.length -1;
+      }
+      canvas.style.background = `url(./images/${images[i]})`;
+    } else if (e.target.id === "right") {
+      i++;
+      if (i >= images.length ){
+        i = 0;
+      }
+      canvas.style.background = `url(./images/${images[i]})`;
+    }
+  })
 });
