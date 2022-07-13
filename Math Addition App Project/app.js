9dca21d580a3e5778159848b751407d06119f6ab
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-13 12:01:34 -0400

    add condition

--- Math Addition App Project/app.js
+++ Math Addition App Project/app.js
@@ -19,5 +19,12 @@ button.addEventListener('click', function(){
 
   let guess = document.getElementById('guess').value;
       guess = Number(guess);
-  
+  //check answer
+  if (guess === total){
+    alert('Correct');
+    window.location.reload()
+  } else {
+    alert('Sorry. Incorrect. The correct answer was ' + total + '.')
+    window.location.reload()
+  }
 });
