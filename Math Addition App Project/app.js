f78dc6edcdb4fa37e68ceb79b4490c8eb6ee98ff
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-13 11:58:04 -0400

    create primary

--- Math Addition App Project/app.js
+++ Math Addition App Project/app.js
@@ -4,3 +4,6 @@ let secondNumber = parseInt(Math.random()*10);
 
 //get the total
 let total = firstNumber + secondNumber;
+
+//display numbers on the canvas
+let primary = document.getElementById('primary-number');
