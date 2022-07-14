0b7c2efc1eb403e82d65bf61f6e2acaa62338b70
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-14 14:18:13 -0400

    create payment

--- Mortgage Loan Calculator/loancalculator.js
+++ Mortgage Loan Calculator/loancalculator.js
@@ -3,4 +3,6 @@ function computeLoan(){
   const interest_rate = document.querySelector('#interest_rate').value;
   const months = document.querySelector('#months').value;
   const interest = (amount * (interest_rate * 0.01)) / months;
+  let payment = ((amount / months) + interest).toFixed(2); //calculate monthly payment
+  
 }
