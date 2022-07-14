b08b9046c556a71d3fab053fe2c1399f9863e945
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-14 14:17:17 -0400

    create months

--- Mortgage Loan Calculator/loancalculator.js
+++ Mortgage Loan Calculator/loancalculator.js
@@ -1,5 +1,6 @@
 function computeLoan(){
   const amount = document.querySelector('#amount').value;
   const interest_rate = document.querySelector('#interest_rate').value;
+  const months = document.querySelector('#months').value;
   
 }
