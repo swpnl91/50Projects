6b752b0859ed6efe50abfcf7c01eee612ffa51f5
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-15 11:08:50 -0400

    create guessSlot

--- Number Guessing Game/app.js
+++ Number Guessing Game/app.js
@@ -2,3 +2,4 @@
 let randomNumber = parseInt((Math.random()*100)+1);
 const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
