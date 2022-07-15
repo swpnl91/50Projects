febf0e2203af846f4a80bf754c2f9a781b46c760
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-15 11:14:47 -0400

    create function validateGuess

--- Number Guessing Game/app.js
+++ Number Guessing Game/app.js
@@ -19,3 +19,27 @@ if (playGame){
       validateGuess(guess);
   });
 }
+
+function validateGuess(guess){
+  if (isNaN(guess)){
+      alert('Please enter a valid number');
+  } else if (guess < 1) {
+      alert('Please enter a number greater than 1!');
+  } else if (guess > 100){
+      alert('Please enter a number less than 500!')
+  } else {
+      //Keep record of number of attempted guesses
+      previousGuesses.push(guess);
+      //Check to see if game is over
+      if (numGuesses === 11){
+          displayGuesses(guess);
+          displayMessage(`Game Over! Number was ${randomNumber}`);
+          endGame();
+      } else {
+      //Display previous guessed numbers
+      displayGuesses(guess);
+      //Check guess and display if wrong
+      checkGuess(guess);
+      }
+  }
+}
