5f95dd6db6a030b0d6b1d81b8097bd41447b030f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-15 11:09:15 -0400

    create remaining

--- Number Guessing Game/app.js
+++ Number Guessing Game/app.js
@@ -3,3 +3,4 @@ let randomNumber = parseInt((Math.random()*100)+1);
 const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
 const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
