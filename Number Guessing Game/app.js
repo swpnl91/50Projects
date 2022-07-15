7b0c1a3f50d51e6b92930dcb333b2237d2165de8
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-15 11:08:25 -0400

    create userInput

--- Number Guessing Game/app.js
+++ Number Guessing Game/app.js
@@ -1,3 +1,4 @@
 //Generate a random number between 1 and 500
 let randomNumber = parseInt((Math.random()*100)+1);
 const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
