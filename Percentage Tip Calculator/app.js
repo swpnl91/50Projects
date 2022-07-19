8d8b7aeb67db48835509151974472068557be8ba
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-19 14:24:59 -0400

    add event listener for calculate

--- Percentage Tip Calculator/app.js
+++ Percentage Tip Calculator/app.js
@@ -3,3 +3,9 @@ let percentTip = document.getElementById('percentage-tip');
 let tipAmount = document.getElementById('tip-amount');
 let total = document.getElementById('total');
 let calculate = document.getElementById('calculate');
+
+calculate.addEventListener('click', function(){
+  
+  
+
+})
