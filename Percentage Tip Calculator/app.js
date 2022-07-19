83c4872b8ea9f21e13cd9f127e248de3bff753a6
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-19 14:26:34 -0400

    add conditions

--- Percentage Tip Calculator/app.js
+++ Percentage Tip Calculator/app.js
@@ -7,6 +7,18 @@ let calculate = document.getElementById('calculate');
 calculate.addEventListener('click', function(){
   billAmount = Number(billAmount.value);
   percentTip = Number(percentTip.value);
+  
+  if (isNaN(billAmount) || billAmount <= 0 || billAmount === null){
+    alert('Please enter a valid Bill Amount')
+  }
+  else if (isNaN(percentTip) || percentTip <= 0 || percentTip === null){
+      alert('Please enter a valid Tip Percentage')
+  }
+  else {
+      let calculatedTip =  percentTip/100*billAmount;
+      tipAmount.value = '$' + calculatedTip;
 
-
+      let calculatedTotal = billAmount + calculatedTip;
+      total.value = '$' + calculatedTotal;
+  }
 })
