71d9c649c4cf07d58bd071d6293f92895caa9e14
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-19 14:24:14 -0400

    create calculate

--- Percentage Tip Calculator/app.js
+++ Percentage Tip Calculator/app.js
@@ -2,3 +2,4 @@ let billAmount = document.getElementById('bill-amount');
 let percentTip = document.getElementById('percentage-tip');
 let tipAmount = document.getElementById('tip-amount');
 let total = document.getElementById('total');
+let calculate = document.getElementById('calculate');
