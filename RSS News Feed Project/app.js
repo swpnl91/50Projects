79e84833f73296dffa47f01377f908cdcc1dbbff
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-21 06:13:09 -0400

    set innerHTML

--- RSS News Feed Project/app.js
+++ RSS News Feed Project/app.js
@@ -2,3 +2,5 @@ let url = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
 const textarea = document.querySelector('#feed-textarea > ul');
 
 const date = new Date();
+
+document.querySelector('#date').innerHTML = date.toDateString();
