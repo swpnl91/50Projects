cc707d2b0e7de4e721beeee71dac8af7580bd385
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-22 06:16:29 -0400

    create function get_todos

---
+++ TodoList Project BlueLime Solution/app.js
@@ -0,0 +1,9 @@
+//retrieve data from local storage
+function get_todos(){
+  var todos = new Array;
+  var todos_str = localStorage.getItem('todo');
+  if(todos_str !== null){
+      todos = JSON.parse(todos_str);
+  }
+  return todos;
+}
