4c05b947b517bbf392b07cf957a8b355285e1128
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-25 06:01:52 -0400

    create timeUp

--- whack-a-mole/script.js
+++ whack-a-mole/script.js
@@ -2,3 +2,4 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
