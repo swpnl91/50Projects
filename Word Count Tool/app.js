57381ba210cb38dbdddd16b621d1693584fc09d2
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-07-27 06:12:58 -0400

    create outputDiv

--- Word Count Tool/app.js
+++ Word Count Tool/app.js
@@ -3,5 +3,7 @@ let button = document.getElementById('btn');
 button.addEventListener('click', function(){
   let word = document.getElementById('str').value;
   let count = word.length;
+  let outputDiv = document.getElementById('output');
+
   
 });
