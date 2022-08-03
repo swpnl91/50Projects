53570399a72c33d0e14df05fa8bb80dd8733bfbd
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-03 07:31:42 -0400

    create function resetDOM

--- Practice/animated-countdown/script.js
+++ Practice/animated-countdown/script.js
@@ -4,3 +4,14 @@ const finalMessage = document.querySelector('.final')
 const replay = document.querySelector('#replay')
 
 runAnimation()
+
+function resetDOM() {
+  counter.classList.remove('hide')
+  finalMessage.classList.remove('show')
+
+  nums.forEach((num) => {
+    num.classList.value = ''
+  })
+
+  nums[0].classList.add('in')
+}
