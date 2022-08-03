ab7e7fa6381cf8456c49a6eb1e73b1cb51bbcd69
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-03 07:31:11 -0400

    call runAnimation

--- Practice/animated-countdown/script.js
+++ Practice/animated-countdown/script.js
@@ -2,3 +2,5 @@ const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
 const finalMessage = document.querySelector('.final')
 const replay = document.querySelector('#replay')
+
+runAnimation()
