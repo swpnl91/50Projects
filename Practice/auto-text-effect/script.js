055fe2187634b3ec1e5ac707f767b290ac85c806
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-05 06:17:33 -0400

    create function writeText

--- Practice/auto-text-effect/script.js
+++ Practice/auto-text-effect/script.js
@@ -5,3 +5,15 @@ let idx = 1
 let speed = 300 / speedEl.value
 
 writeText()
+
+function writeText() {
+  textEl.innerText = text.slice(0, idx)
+
+  idx++
+
+  if(idx > text.length) {
+      idx = 1
+  }
+
+  setTimeout(writeText, speed)
+}
