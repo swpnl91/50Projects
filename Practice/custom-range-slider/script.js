9ed422e6e5a931111ef3b878a8ca254f2cf77e7d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-16 11:23:21 -0400

    create range_width

--- Practice/custom-range-slider/script.js
+++ Practice/custom-range-slider/script.js
@@ -4,5 +4,6 @@ range.addEventListener('input', (e) => {
   const value = +e.target.value
   const label = e.target.nextElementSibling
 
+  const range_width = getComputedStyle(e.target).getPropertyValue('width')
   
 })
