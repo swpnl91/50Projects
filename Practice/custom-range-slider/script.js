105bd83fafbfbfdafbc2aa95a22c3c96a3c8a3dd
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-16 11:27:39 -0400

    map a range of numbers to another range of numbers

--- Practice/custom-range-slider/script.js
+++ Practice/custom-range-slider/script.js
@@ -19,3 +19,8 @@ range.addEventListener('input', (e) => {
 
   label.innerHTML = value
 })
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
