d73c9b49779858f323080b5a187f6fcc30b31313
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-24 17:17:26 -0400

    create slideLeft

--- Practice/double-vertical-slider/script.js
+++ Practice/double-vertical-slider/script.js
@@ -1,2 +1,3 @@
 const sliderContainer = document.querySelector('.slider-container')
 const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
