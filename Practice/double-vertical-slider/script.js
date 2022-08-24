f3ef1fc92993a1e972340959cb6971eb8526ec8a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-24 17:17:51 -0400

    create upButton

--- Practice/double-vertical-slider/script.js
+++ Practice/double-vertical-slider/script.js
@@ -1,3 +1,4 @@
 const sliderContainer = document.querySelector('.slider-container')
 const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
