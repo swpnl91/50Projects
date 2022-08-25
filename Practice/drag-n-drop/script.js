03d11d5d344dc7f3b629e04e334374d290636dce
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-25 05:41:56 -0400

    add end event listener to fill

--- Practice/drag-n-drop/script.js
+++ Practice/drag-n-drop/script.js
@@ -2,3 +2,4 @@ const fill = document.querySelector('.fill')
 const empties = document.querySelectorAll('.empty')
 
 fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
