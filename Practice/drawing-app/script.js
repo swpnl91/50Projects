aee420cfe9c9bca8741d47445e5a86c327033c92
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-26 08:17:52 -0400

    create function drawLine

--- Practice/drawing-app/script.js
+++ Practice/drawing-app/script.js
@@ -47,3 +47,12 @@ function drawCircle(x, y) {
   ctx.fillStyle = color
   ctx.fill()
 }
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.strokeStyle = color
+  ctx.lineWidth = size * 2
+  ctx.stroke()
+}
