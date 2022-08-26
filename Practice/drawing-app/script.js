e7ab916cf9ba6b24b6e9bcafc1957ef99a568282
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-26 08:12:02 -0400

    create clearEl

--- Practice/drawing-app/script.js
+++ Practice/drawing-app/script.js
@@ -3,3 +3,4 @@ const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 const sizeEL = document.getElementById('size');
 const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
