b6522ad965c395e2da8cef1ed8aed49292611d23
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-30 05:51:08 -0400

    create function highlightCups

--- Practice/drink-water/script.js
+++ Practice/drink-water/script.js
@@ -8,3 +8,20 @@ updateBigCup()
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlightCups(idx))
 })
+
+function highlightCups(idx) {
+  if (idx===7 && smallCups[idx].classList.contains("full")) idx--;
+  else if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+      idx--
+  }
+
+  smallCups.forEach((cup, idx2) => {
+      if(idx2 <= idx) {
+          cup.classList.add('full')
+      } else {
+          cup.classList.remove('full')
+      }
+  })
+
+  updateBigCup()
+}
