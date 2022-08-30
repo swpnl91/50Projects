eaf4dc5d1731bc4c6627b4db23b8eb349844cdc1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-30 05:50:34 -0400

    iterate through smallCups

--- Practice/drink-water/script.js
+++ Practice/drink-water/script.js
@@ -4,3 +4,7 @@ const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
 updateBigCup()
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCups(idx))
+})
