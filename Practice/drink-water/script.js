5f3e3771961b53c2a7c0f7c4a4f575f6d5adf841
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-08-30 05:50:09 -0400

    call updateBigCup

--- Practice/drink-water/script.js
+++ Practice/drink-water/script.js
@@ -2,3 +2,5 @@ const smallCups = document.querySelectorAll('.cup-small')
 const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
+
+updateBigCup()
