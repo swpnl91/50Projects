768a2f588ae2965c301d20d8280eb0fda130c025
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-05 10:59:12 -0400

    create selectedRating

--- Practice/feedback-ui-design/script.js
+++ Practice/feedback-ui-design/script.js
@@ -2,3 +2,4 @@ const ratings = document.querySelectorAll('.rating')
 const ratingsContainer = document.querySelector('.ratings-container')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
