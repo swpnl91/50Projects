0fab53b09fe08f4d59be630dfe7e30057ded6add
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-05 11:00:03 -0400

    add event listener to ratingsContainer

--- Practice/feedback-ui-design/script.js
+++ Practice/feedback-ui-design/script.js
@@ -3,3 +3,20 @@ const ratingsContainer = document.querySelector('.ratings-container')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
 let selectedRating = 'Satisfied'
+
+ratingsContainer.addEventListener('click', (e) => {
+  if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
+      removeActive()
+      e.target.parentNode.classList.add('active')
+      selectedRating = e.target.nextElementSibling.innerHTML
+  } else if(
+      e.target.parentNode.classList.contains('rating') &&
+      e.target.previousSibling &&
+      e.target.previousElementSibling.nodeName === 'IMG'
+  ) {
+      removeActive()
+      e.target.parentNode.classList.add('active')
+      selectedRating = e.target.innerHTML
+  }
+
+})
