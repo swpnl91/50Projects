15d9d70ad0d7396a0fe3c8d31c09d528697d5b19
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-05 10:58:49 -0400

    create panel

--- Practice/feedback-ui-design/script.js
+++ Practice/feedback-ui-design/script.js
@@ -1,3 +1,4 @@
 const ratings = document.querySelectorAll('.rating')
 const ratingsContainer = document.querySelector('.ratings-container')
 const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
