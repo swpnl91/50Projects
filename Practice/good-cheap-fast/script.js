cd85072737af09ff9c91559de295aa4cc6556876
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-12 14:54:50 -0400

    iterate over toggles

--- Practice/good-cheap-fast/script.js
+++ Practice/good-cheap-fast/script.js
@@ -2,3 +2,5 @@ const toggles = document.querySelectorAll('.toggle')
 const good = document.querySelector('#good')
 const cheap = document.querySelector('#cheap')
 const fast = document.querySelector('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
