bcdfe7fe2e799fbfed12b9fe2fd4867ae2fe264f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-12 14:55:18 -0400

    create function doTheTrick

--- Practice/good-cheap-fast/script.js
+++ Practice/good-cheap-fast/script.js
@@ -4,3 +4,19 @@ const cheap = document.querySelector('#cheap')
 const fast = document.querySelector('#fast')
 
 toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(theClickedOne) {
+  if(good.checked && cheap.checked && fast.checked) {
+      if(good === theClickedOne) {
+          fast.checked = false
+      }
+
+      if(cheap === theClickedOne) {
+          good.checked = false
+      }
+
+      if(fast === theClickedOne) {
+          cheap.checked = false
+      }
+  }
+}
