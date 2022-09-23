7d421472d876db9605aa5ca644f08a97104db469
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-23 18:27:09 -0400

    create timeEl

--- Practice/insect-catch-game/script.js
+++ Practice/insect-catch-game/script.js
@@ -2,3 +2,4 @@ const screens = document.querySelectorAll('.screen');
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
 const start_btn = document.getElementById('start-btn')
 const game_container = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
