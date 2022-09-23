2859e811eefc048884a86fcf3e2a82284dcaaac0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-23 18:32:45 -0400

    create function getRandomLocation

--- Practice/insect-catch-game/script.js
+++ Practice/insect-catch-game/script.js
@@ -48,3 +48,11 @@ function createInsect() {
 
   game_container.appendChild(insect)
 }
+
+function getRandomLocation() {
+  const width = window.innerWidth
+  const height = window.innerHeight
+  const x = Math.random() * (width - 200) + 100
+  const y = Math.random() * (height - 200) + 100
+  return { x, y }
+}
