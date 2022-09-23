a48ba858a1f23cc31e8b8122462758ac4552cd97
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-23 18:31:01 -0400

    iterate over choose_insect_btns

--- Practice/insect-catch-game/script.js
+++ Practice/insect-catch-game/script.js
@@ -10,3 +10,15 @@ let score = 0
 let selected_insect = {}
 
 start_btn.addEventListener('click', () => screens[0].classList.add('up'))
+
+choose_insect_btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+      const img = btn.querySelector('img')
+      const src = img.getAttribute('src')
+      const alt = img.getAttribute('alt')
+      selected_insect = { src, alt }
+      screens[1].classList.add('up')
+      setTimeout(createInsect, 1000)
+      startGame()
+  })
+})
