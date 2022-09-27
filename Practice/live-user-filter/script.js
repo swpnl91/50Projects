085cf6398de8cb7c9351e2b5173c2610ef939b9d
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-27 06:00:14 -0400

    create function getData

--- Practice/live-user-filter/script.js
+++ Practice/live-user-filter/script.js
@@ -5,3 +5,28 @@ const listItems = []
 getData()
 
 filter.addEventListener('input', (e) => filterData(e.target.value))
+
+async function getData() {
+  const res = await fetch('https://randomuser.me/api?results=50')
+
+  const { results } = await res.json()
+
+  // Clear result
+  result.innerHTML = ''
+
+  results.forEach(user => {
+      const li = document.createElement('li')
+
+      listItems.push(li)
+
+      li.innerHTML = `
+          <img src="${user.picture.large}" alt="${user.name.first}">
+          <div class="user-info">
+              <h4>${user.name.first} ${user.name.last}</h4>
+              <p>${user.location.city}, ${user.location.country}</p>
+          </div>
+      `
+
+      result.appendChild(li)
+  })
+}
