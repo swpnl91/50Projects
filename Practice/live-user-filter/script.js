6662b6af3472a464d880c2cea12d1094d7155260
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-27 06:00:43 -0400

    create function filterData

--- Practice/live-user-filter/script.js
+++ Practice/live-user-filter/script.js
@@ -30,3 +30,13 @@ async function getData() {
       result.appendChild(li)
   })
 }
+
+function filterData(searchTerm) {
+  listItems.forEach(item => {
+      if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+          item.classList.remove('hide')
+      } else {
+          item.classList.add('hide')
+      }
+  })
+}
