26808de292e767eda5bbecff5f4a4aa387bac4fc
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-30 06:26:40 -0400

    create form

--- Practice/movie-app/script.js
+++ Practice/movie-app/script.js
@@ -3,3 +3,4 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 const main = document.getElementById('main')
+const form = document.getElementById('form')
