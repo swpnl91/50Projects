2efa2219a99f3a4c83550eda322be339da644ae8
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-09-30 06:28:25 -0400

    create function showMovies

--- Practice/movie-app/script.js
+++ Practice/movie-app/script.js
@@ -15,3 +15,27 @@ async function getMovies(url) {
 
   showMovies(data.results)
 }
+
+function showMovies(movies) {
+  main.innerHTML = ''
+
+  movies.forEach((movie) => {
+      const { title, poster_path, vote_average, overview } = movie
+
+      const movieEl = document.createElement('div')
+      movieEl.classList.add('movie')
+
+      movieEl.innerHTML = `
+          <img src="${IMG_PATH + poster_path}" alt="${title}">
+          <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+          </div>
+          <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+      </div>
+      `
+      main.appendChild(movieEl)
+  })
+}
