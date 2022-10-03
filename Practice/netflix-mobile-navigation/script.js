1e0cffcc4f321c424d2e3ae307ae9f04f79a97c4
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-10-03 06:15:10 -0400

    add event listener to close_btn

--- Practice/netflix-mobile-navigation/script.js
+++ Practice/netflix-mobile-navigation/script.js
@@ -5,3 +5,7 @@ const nav = document.querySelectorAll('.nav')
 open_btn.addEventListener('click', () => {
   nav.forEach(nav_el => nav_el.classList.add('visible'))
 })
+
+close_btn.addEventListener('click', () => {
+  nav.forEach(nav_el => nav_el.classList.remove('visible'))
+})
