a97ca2a8765c1555ead3d893a82618634f6b142e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-10-06 07:04:10 -0400

    create function getRandomUpper

--- Practice/password-generator/script.js
+++ Practice/password-generator/script.js
@@ -57,3 +57,7 @@ function generatePassword(lower, upper, number, symbol, length) {
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
