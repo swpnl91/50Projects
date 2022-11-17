c969a4f3f035b112c07260df8ee8436b79db971b
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-11-17 12:53:29 -0500

    create mouseenter event listener for right

--- Practice/split-landing-page/script.js
+++ Practice/split-landing-page/script.js
@@ -5,3 +5,5 @@ const container = document.querySelector('.container')
 
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
