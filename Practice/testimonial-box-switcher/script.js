5f278837e84f1ef5d61edfb6459179ad308a2601
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-11-22 16:42:48 -0500

    create userImage

--- Practice/testimonial-box-switcher/script.js
+++ Practice/testimonial-box-switcher/script.js
@@ -1,2 +1,3 @@
 const testimonialsContainer = document.querySelector('.testimonials-container')
 const testimonial = document.querySelector('.testimonial')
+const userImage = document.querySelector('.user-image')
