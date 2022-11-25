af2eaa63ad048c0b6be6259814e8ec4dbd683963
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-11-24 19:10:29 -0500

    create timeEl

--- Practice/theme-clock/script.js
+++ Practice/theme-clock/script.js
@@ -1,3 +1,4 @@
 const hourEl = document.querySelector('.hour')
 const minuteEl = document.querySelector('.minute')
 const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
