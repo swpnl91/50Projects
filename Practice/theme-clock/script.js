f7b8265df105173795dbfb5a0fe9c313493093a3
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-11-24 19:12:07 -0500

    create months

--- Practice/theme-clock/script.js
+++ Practice/theme-clock/script.js
@@ -6,3 +6,4 @@ const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
