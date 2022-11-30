197da838f6c84cebd51d8df5436eb4fa1882d7bd
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-11-30 10:10:15 -0500

    add condition

--- Practice/todo-list/script.js
+++ Practice/todo-list/script.js
@@ -3,3 +3,7 @@ const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
 
 const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+  todos.forEach(todo => addTodo(todo))
+}
