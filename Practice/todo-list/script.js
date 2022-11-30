6329fdced5e1761f347916be08ab4ea40f3a7bc4
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-11-30 10:11:11 -0500

    create function addTodo

--- Practice/todo-list/script.js
+++ Practice/todo-list/script.js
@@ -13,3 +13,38 @@ form.addEventListener('submit', (e) => {
 
   addTodo()
 })
+
+function addTodo(todo) {
+  let todoText = input.value
+
+  if(todo) {
+      todoText = todo.text
+  }
+
+  if(todoText) {
+      const todoEl = document.createElement('li')
+      if(todo && todo.completed) {
+          todoEl.classList.add('completed')
+      }
+
+      todoEl.innerText = todoText
+
+      todoEl.addEventListener('click', () => {
+          todoEl.classList.toggle('completed')
+          updateLS()
+      }) 
+
+      todoEl.addEventListener('contextmenu', (e) => {
+          e.preventDefault()
+
+          todoEl.remove()
+          updateLS()
+      }) 
+
+      todosUL.appendChild(todoEl)
+
+      input.value = ''
+
+      updateLS()
+  }
+}
