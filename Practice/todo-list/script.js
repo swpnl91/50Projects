6497c14d5eecc96a3cb0f4977d711a8a1e5e1664
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-11-30 10:09:49 -0500

    create todos

--- Practice/todo-list/script.js
+++ Practice/todo-list/script.js
@@ -2,3 +2,4 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
