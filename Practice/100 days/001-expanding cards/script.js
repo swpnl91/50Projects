9d7c842e2358169eecee26fdaa6b8823d92223c6
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-12-06 08:59:37 -0500

    addEventListener to panels

--- Practice/100 days/001-expanding cards/script.js
+++ Practice/100 days/001-expanding cards/script.js
@@ -1,5 +1,8 @@
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
-  
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
 });
