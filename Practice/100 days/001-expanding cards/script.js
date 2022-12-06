be240d3d1a903dee657c1e26809490281117385e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-12-06 09:00:09 -0500

    create function const removeActiveClasses

--- Practice/100 days/001-expanding cards/script.js
+++ Practice/100 days/001-expanding cards/script.js
@@ -6,3 +6,9 @@ panels.forEach((panel) => {
     panel.classList.add("active");
   });
 });
+
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
