31d7b54edf12edd31c768fbe7780d6a965fa263c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-12-09 08:02:48 -0500

    addEventListener to open

--- Practice/100 days/003-rotating navigation/script.js
+++ Practice/100 days/003-rotating navigation/script.js
@@ -1,3 +1,5 @@
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const container = document.querySelector(".container");
+
+open.addEventListener("click", () => container.classList.add("show-nav"));
