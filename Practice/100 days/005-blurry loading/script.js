04a6a60a9d1f2904748a7e8bfc43aacf18f9004e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-12-15 09:59:06 -0500

    create function blurring

--- Practice/100 days/005-blurry loading/script.js
+++ Practice/100 days/005-blurry loading/script.js
@@ -2,3 +2,11 @@ const loadText = document.querySelector(".loading-text");
 const bg = document.querySelector(".bg");
 
 let load = 0;
+
+const blurring = () => {
+  load++;
+  if (load > 99) clearInterval(int);
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
