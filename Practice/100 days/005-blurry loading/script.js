3bdc2a3cdf4cc0d51ab272eff9fad9e1ff75d470
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-12-15 09:59:43 -0500

    create function scale

--- Practice/100 days/005-blurry loading/script.js
+++ Practice/100 days/005-blurry loading/script.js
@@ -10,3 +10,8 @@ const blurring = () => {
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 };
+
+// For reference: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
