fb6f69b1e76f6e8627d66fc876c224ec60a11b6e
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-12-15 10:00:13 -0500

    create int

--- Practice/100 days/005-blurry loading/script.js
+++ Practice/100 days/005-blurry loading/script.js
@@ -15,3 +15,5 @@ const blurring = () => {
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
+let int = setInterval(blurring, 30);
