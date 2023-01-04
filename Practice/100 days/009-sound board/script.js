2b18533f1fdbb48d0dce71bb5f51227ff5de15c9
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-01-04 14:18:17 -0500

    create function stopSounds

--- Practice/100 days/009-sound board/script.js
+++ Practice/100 days/009-sound board/script.js
@@ -1,2 +1,10 @@
 const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 const buttons = document.getElementById("buttons");
+
+const stopSounds = () => {
+  sounds.forEach((sound) => {
+    const currentSound = document.getElementById(sound);
+    currentSound.pause();
+    currentSound.currentTime = 0;
+  });
+};
