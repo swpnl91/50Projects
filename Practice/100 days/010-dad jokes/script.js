c32d5c6c9a9d64b3732aa774795c1a83030f916f
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-01-10 18:07:18 -0500

    create config

--- Practice/100 days/010-dad jokes/script.js
+++ Practice/100 days/010-dad jokes/script.js
@@ -2,5 +2,13 @@ const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokeBtn");
 
 const generateJoke = async () => {
+  const config = {
+    headers: { Accept: "application/json" },
+  };
   
+
+  // Fetching with .then()
+  //   fetch("https://icanhazdadjoke.com/", config)
+  //     .then((res) => res.json())
+  //     .then((data) => (jokeEl.innerHTML = data.joke));
 };
