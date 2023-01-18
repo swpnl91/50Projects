58d4aab7872a716739f47ffdc87f7b6032adabca
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-01-18 16:28:21 -0500

    create function randomSelect

--- Practice/100 days/013-random choice picker/script.js
+++ Practice/100 days/013-random choice picker/script.js
@@ -24,3 +24,21 @@ const highlightTag = (tag) => tag.classList.add("highlight");
 
 const unHighlightTag = (tag) => tag.classList.remove("highlight");
 
+const randomSelect = () => {
+  const times = 30;
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlightTag(randomTag);
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+};
