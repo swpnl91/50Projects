255d3390350183e2d25b13f745f522dbecdadae0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-01-18 16:26:33 -0500

    create function createTags

--- Practice/100 days/013-random choice picker/script.js
+++ Practice/100 days/013-random choice picker/script.js
@@ -1,2 +1,16 @@
 const tagsElements = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
+
+const createTags = (input) => {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  tagsElements.innerHTML = "";
+  tags.forEach((tag) => {
+    const tagElement = document.createElement("span");
+    tagElement.classList.add("tag");
+    tagElement.innerText = tag;
+    tagsElements.appendChild(tagElement);
+  });
+};
