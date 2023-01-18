cd9d5bb51f43f9bc0c58ee36d445127e4411d5cb
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-01-18 16:28:53 -0500

    add addEventListener to textarea

--- Practice/100 days/013-random choice picker/script.js
+++ Practice/100 days/013-random choice picker/script.js
@@ -42,3 +42,11 @@ const randomSelect = () => {
     }, 100);
   }, times * 100);
 };
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+  if (e.key === "Enter") {
+    setTimeout(() => (e.target.value = ""), 10);
+    randomSelect();
+  }
+});
