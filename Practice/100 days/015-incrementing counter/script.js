9dbbd96edee45c8c055121f4dec39ac7d44be80c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-04 15:58:23 -0400

    create target, count, increment

--- Practice/100 days/015-incrementing counter/script.js
+++ Practice/100 days/015-incrementing counter/script.js
@@ -3,6 +3,9 @@ const counters = document.querySelectorAll(".counter");
 counters.forEach((counter) => {
   counter.innerText = "0";
   const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
 
   };
 
