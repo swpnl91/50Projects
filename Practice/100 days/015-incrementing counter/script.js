caffb5400c386e7ebc1a4729b7bbeb59e1ff8ae7
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-04 16:13:47 -0400

    add condition

--- Practice/100 days/015-incrementing counter/script.js
+++ Practice/100 days/015-incrementing counter/script.js
@@ -6,7 +6,10 @@ counters.forEach((counter) => {
     const target = +counter.getAttribute("data-target");
     const count = +counter.innerText;
     const increment = target / 200;
-
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
   };
 
 });
