c5195e2ada902bcbe1a4c45fa093d3c28e0c9d35
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-10 14:33:11 -0400

    add conditions

--- Practice/100 days/016-drink water/script.js
+++ Practice/100 days/016-drink water/script.js
@@ -6,5 +6,19 @@ const remained = document.getElementById("remained");
 const updateBigCup = () => {
   const fullCups = document.querySelectorAll(".cup-small.full").length;
   const totalCups = smallCups.length;
-
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 };
