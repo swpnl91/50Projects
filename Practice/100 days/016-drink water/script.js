f4996f9bf85fe8f9d77a008615b400af1c5beb35
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-10 14:32:52 -0400

    create function updateBigCup

--- Practice/100 days/016-drink water/script.js
+++ Practice/100 days/016-drink water/script.js
@@ -2,3 +2,9 @@ const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+
+const updateBigCup = () => {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+};
