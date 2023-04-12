c75549f8cd2ad14a872791e5fb69aec434478555
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-12 13:23:33 -0400

    create main

--- Practice/100 days/017-movie app/script.js
+++ Practice/100 days/017-movie app/script.js
@@ -2,3 +2,5 @@ const KEY = "3fd2be6f0c70a2a598f084ddfb75487c";
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEY}&page=1`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=`;
+
+const main = document.getElementById("main");
