b41103d8f1620046660da50e3b425b02f71218ce
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-12 13:23:59 -0400

    create form

--- Practice/100 days/017-movie app/script.js
+++ Practice/100 days/017-movie app/script.js
@@ -4,3 +4,4 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=`;
 
 const main = document.getElementById("main");
+const form = document.getElementById("form");
