184a9dd0f450bb2424f41b6fd2ab4198d86623b8
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-14 12:53:53 -0400

    create activeSlide

--- Practice/100 days/018-background slider/script.js
+++ Practice/100 days/018-background slider/script.js
@@ -2,3 +2,5 @@ const body = document.body;
 const slides = document.querySelectorAll(".slide");
 const leftButton = document.getElementById("left");
 const rightButton = document.getElementById("right");
+
+let activeSlide = 0;
