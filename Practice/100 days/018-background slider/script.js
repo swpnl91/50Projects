f92fe28c1b8a1ec22c037198bcbcdb5647bce760
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-14 12:53:28 -0400

    create rightButton

--- Practice/100 days/018-background slider/script.js
+++ Practice/100 days/018-background slider/script.js
@@ -1,3 +1,4 @@
 const body = document.body;
 const slides = document.querySelectorAll(".slide");
 const leftButton = document.getElementById("left");
+const rightButton = document.getElementById("right");
