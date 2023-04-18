b95e06a28c7fdee159a6da1dba43ff86f7698057
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-18 16:10:07 -0400

    add event listener to toggle

--- Practice/100 days/19-theme clock/script.js
+++ Practice/100 days/19-theme clock/script.js
@@ -29,3 +29,14 @@ const months = [
   "Nov",
   "Dec",
 ];
+
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light mode";
+  }
+});
