28f10e2129622015cab603758509dd7c3e32bdd0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-18 16:08:56 -0400

    create toggle

--- Practice/100 days/19-theme clock/script.js
+++ Practice/100 days/19-theme clock/script.js
@@ -3,3 +3,4 @@ const minuteElement = document.querySelector(".minute");
 const secondElement = document.querySelector(".second");
 const timeElement = document.querySelector(".time");
 const dateElement = document.querySelector(".date");
+const toggle = document.querySelector(".toggle");
