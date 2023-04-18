b1c8a2c54607ce02bad415d7bcb92b0a61f25b78
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-18 16:11:16 -0400

    create function setTime

--- Practice/100 days/19-theme clock/script.js
+++ Practice/100 days/19-theme clock/script.js
@@ -45,3 +45,42 @@ toggle.addEventListener("click", (e) => {
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
+const setTime = () => {
+  const time = new Date();
+  const date = time.getDate();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const hours = time.getHours();
+  const hoursForClock = hours >= 13 ? hours % 12 : hours;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+  minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+  secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  timeElement.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+  dateElement.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+};
