da9ed729412cb1b9f52ca507d64b3ef0f1c4216a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-19 15:03:43 -0400

    create y

--- Practice/100 days/020-button ripple effect/script.js
+++ Practice/100 days/020-button ripple effect/script.js
@@ -3,6 +3,7 @@ const buttons = document.querySelectorAll(".ripple");
 buttons.forEach((button) => {
   button.addEventListener("click", function (e) {
     const x = e.clientX;
+    const y = e.clientY;
     
   });
 });
