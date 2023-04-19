54977862542f3152fedabff4953ea622269f693c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-19 15:04:02 -0400

    create buttonTop

--- Practice/100 days/020-button ripple effect/script.js
+++ Practice/100 days/020-button ripple effect/script.js
@@ -4,6 +4,7 @@ buttons.forEach((button) => {
   button.addEventListener("click", function (e) {
     const x = e.clientX;
     const y = e.clientY;
+    const buttonTop = e.target.offsetTop;
     
   });
 });
