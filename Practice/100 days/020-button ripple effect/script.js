2d0a94d5b2d5cde4eeaf11ad2c032056e3a75ed1
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-19 15:05:00 -0400

    create yInside

--- Practice/100 days/020-button ripple effect/script.js
+++ Practice/100 days/020-button ripple effect/script.js
@@ -7,6 +7,7 @@ buttons.forEach((button) => {
     const buttonTop = e.target.offsetTop;
     const buttonLeft = e.target.offsetLeft;
     const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
     
   });
 });
