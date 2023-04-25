88616faeeb3e129ad5bf7e174a76e09e431ee4ab
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-25 05:58:38 -0400

    create colorElement

--- Practice/100 days/022-drawing app/script.js
+++ Practice/100 days/022-drawing app/script.js
@@ -3,3 +3,4 @@ const canvas = document.getElementById("canvas");
 const increaseButton = document.getElementById("increase");
 const decreaseButton = document.getElementById("decrease");
 const sizeElement = document.getElementById("size");
+const colorElement = document.getElementById("color");
