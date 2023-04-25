051d53b0327667ad3b28f4e0f4ba96a0d7b998a4
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-25 05:57:52 -0400

    create decreaseButton

--- Practice/100 days/022-drawing app/script.js
+++ Practice/100 days/022-drawing app/script.js
@@ -1,3 +1,4 @@
 // Ref: https://developer.mozilla.org/fr/docs/Web/API/Canvas_API
 const canvas = document.getElementById("canvas");
 const increaseButton = document.getElementById("increase");
+const decreaseButton = document.getElementById("decrease");
