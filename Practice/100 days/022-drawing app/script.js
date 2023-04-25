b21d7dbbcec7adb0292a70aee4db15d46fa1adac
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-04-25 06:02:12 -0400

    create function drawLine

--- Practice/100 days/022-drawing app/script.js
+++ Practice/100 days/022-drawing app/script.js
@@ -19,3 +19,12 @@ const drawCircle = (x, y) => {
   ctx.fillStyle = color;
   ctx.fill();
 };
+
+const drawLine = (x1, y1, x2, y2) => {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
+};
