bf1e27886213742e21aa5f1c227740eb53a16383
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-05-03 05:54:29 -0400

    create slidesLength

--- Practice/100 days/026-double vertical slider/script.js
+++ Practice/100 days/026-double vertical slider/script.js
@@ -3,3 +3,4 @@ const slideRight = document.querySelector(".right-slide");
 const slideLeft = document.querySelector(".left-slide");
 const upButton = document.querySelector(".up-button");
 const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
