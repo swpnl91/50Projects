32a223397c42aa64343b265242de1a48908b269c
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-05-04 06:16:20 -0400

    create function createNotification

--- Practice/100 days/027-toast notification/script.js
+++ Practice/100 days/027-toast notification/script.js
@@ -14,3 +14,12 @@ const getRandomMessage = () =>
   messages[Math.floor(Math.random() * messages.length)];
 
 const getRandomType = () => types[Math.floor(Math.random() * types.length)];
+
+const createNotification = (message = null, type = null) => {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.classList.add(type ? type : getRandomType());
+  notif.innerText = message ? message : getRandomMessage();
+  toasts.appendChild(notif);
+  setTimeout(() => notif.remove(), 3000);
+};
