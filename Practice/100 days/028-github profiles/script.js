e08f48be2562b6623e81697f0d6619c2f8d76485
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-05-08 06:29:05 -0400

    create search

--- Practice/100 days/028-github profiles/script.js
+++ Practice/100 days/028-github profiles/script.js
@@ -1,3 +1,4 @@
 const APIURL = "https://api.github.com/users/";
 const form = document.getElementById("form");
 const main = document.getElementById("main");
+const search = document.getElementById("search");
