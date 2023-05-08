856b9bd6acef03c86649c7bc32b811b6e2eefd60
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-05-08 06:29:43 -0400

    create function createUserCard

--- Practice/100 days/028-github profiles/script.js
+++ Practice/100 days/028-github profiles/script.js
@@ -2,3 +2,31 @@ const APIURL = "https://api.github.com/users/";
 const form = document.getElementById("form");
 const main = document.getElementById("main");
 const search = document.getElementById("search");
+
+const createUserCard = (user) => {
+  const cardHTML = `
+    <div class="card">
+        <div>
+          <img
+            src="${user.avatar_url}"
+            alt="${user.name}"
+            class="avatar"
+          />
+        </div>
+        <div class="user-info">
+          <h2>${user.name}</h2>
+          <p>
+          ${user.bio}
+          </p>
+          <ul>
+            <li>${user.followers}<strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Repos</strong></li>
+          </ul>
+          <div id="repos">
+          </div>
+        </div>
+      </div>
+    `;
+  main.innerHTML = cardHTML;
+};
