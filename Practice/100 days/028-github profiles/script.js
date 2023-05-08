3d5f18664e775dccdc42009b4ce6e5a5ce95b2e0
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-05-08 06:31:44 -0400

    add event listener to form

--- Practice/100 days/028-github profiles/script.js
+++ Practice/100 days/028-github profiles/script.js
@@ -69,3 +69,12 @@ const getRepos = async (username) => {
     createErrorCard("Problem fetching repos");
   }
 };
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const user = search.value;
+  if (user) {
+    getUser(user);
+    search.value = "";
+  }
+});
