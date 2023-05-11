ecab9222a904baa249fa52a5c47b4fe2f2a00094
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-05-11 10:14:47 -0400

    create speed

--- Practice/100 days/030-auto text effect/script.js
+++ Practice/100 days/030-auto text effect/script.js
@@ -2,3 +2,5 @@ const textElement = document.getElementById("text");
 const speedElement = document.getElementById("speed");
 const text = "We Love Programming!";
 let index = 1;
+let speed = 300 / speedElement.value;
+
