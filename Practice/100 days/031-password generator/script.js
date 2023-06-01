5b072952ec31451d445c32249384e596de0f5aa6
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-06-01 14:23:55 -0400

    create function getRandomLower

--- Practice/100 days/031-password generator/script.js
+++ Practice/100 days/031-password generator/script.js
@@ -6,3 +6,10 @@ const numbersElement = document.getElementById("numbers");
 const symbolsElement = document.getElementById("symbols");
 const generateElement = document.getElementById("generate");
 const clipboardElement = document.getElementById("clipboard");
+
+// Random functions
+// fromCharCode: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+// ASCII codes: https://www.w3schools.com/charsets/ref_html_ascii.asp
+const getRandomLower = () =>
+  String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+
