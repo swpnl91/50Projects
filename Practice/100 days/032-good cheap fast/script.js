8fcbb55bccc5a1d4faf6d69b567d45fdf3fd8125
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-06-20 12:46:45 -0400

    add conditions

--- Practice/100 days/032-good cheap fast/script.js
+++ Practice/100 days/032-good cheap fast/script.js
@@ -4,5 +4,9 @@ const cheap = document.getElementById("cheap");
 const fast = document.getElementById("fast");
 
 const doTheTrick = (theClickedOne) => {
-
+  if (good.checked && cheap.checked && fast.checked) {
+    if (good === theClickedOne) fast.checked = false;
+    if (cheap === theClickedOne) good.checked = false;
+    if (fast === theClickedOne) cheap.checked = false;
+  }
 };
