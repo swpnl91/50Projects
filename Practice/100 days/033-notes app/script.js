90fe21f891d06677487503286fe05dafef66b954
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-06-28 14:47:06 -0400

    create function updateLocalStorage

--- Practice/100 days/033-notes app/script.js
+++ Practice/100 days/033-notes app/script.js
@@ -1,2 +1,9 @@
 const addButton = document.getElementById("add");
 const notes = JSON.parse(localStorage.getItem("notes"));
+
+const updateLocalStorage = () => {
+  const notesText = document.querySelectorAll("textarea");
+  const notes = [];
+  notesText.forEach((note) => notes.push(note.value));
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
