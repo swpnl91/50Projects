f5c1c4d96c419c83d2240e2d52f56eae44121c69
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2023-06-28 14:48:47 -0400

    create function addNewNote

--- Practice/100 days/033-notes app/script.js
+++ Practice/100 days/033-notes app/script.js
@@ -7,3 +7,34 @@ const updateLocalStorage = () => {
   notesText.forEach((note) => notes.push(note.value));
   localStorage.setItem("notes", JSON.stringify(notes));
 };
+
+const addNewNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
+  <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+  </div>
+  <div class="main ${text ? "" : "hidden"}"></div>
+  <textarea class="${text ? "hidden" : ""}"></textarea>`;
+
+
+
+  main.innerHTML = marked(text);
+
+  deleteButton.addEventListener("click", () => {
+    note.remove();
+    updateLocalStorage();
+  });
+  editButton.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+    main.innerHTML = marked(value);
+    updateLocalStorage();
+  });
+  document.body.appendChild(note);
+};
